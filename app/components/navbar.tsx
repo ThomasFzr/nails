2716b693f44a44ec5ceaf29bf6c3c7548ae8b937
ThomasFzr <thfltzr@gmail.com>
2025-03-17 13:12:26 +0100

    add routing for price, dispo, and info pages; update Navbar with links and change background color

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,17 +3,20 @@ import { Link } from "react-router";
 export const Navbar = () => {
     return (
         <div className='flex flex-row justify-between w-full z-[100] text-2xl pt-1 text-black items-center'>
-            <div className="w-[30%]"></div>
+            <div className="w-[30%]">
+                <a href="https://www.instagram.com/lesgriffesduz/" target="_blank" rel="noopener noreferrer">
+                    <img className="w-15 pl-4 hover:scale-110" src="/images/insta.png"></img>
+                </a>
+            </div>
             <div>
                 <Link to="/">
-                    <img className="w-30" src="../public/images/logo6.png"></img>
+                    <img className="w-30 hover:scale-110" src="../public/images/logo6.png"></img>
                 </Link>
             </div>
-            {/* <div>G du Z</div> */}
-            <div className="flex flex-row justify-around w-[30%] ">
-                <Link to="/price">PRIX</Link>
-                <div>DISPO</div>
-                <div>INFOS</div>
+            <div className="flex flex-row justify-around w-[30%]">
+                <Link to="/price" className="hover:scale-110">PRIX</Link>
+                <Link to="/dispo" className="hover:scale-110">DISPO</Link>
+                <Link to="/info" className="hover:scale-110">INFOS</Link>
             </div>
 
         </div>
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"), 
+    route("/price", "routes/price.tsx"),
+    route("/dispo", "routes/dispo.tsx"),
+    route("/info", "routes/info.tsx"),
+] satisfies RouteConfig;
