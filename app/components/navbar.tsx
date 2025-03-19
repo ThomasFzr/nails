133dce67ef91c7b3abdd72c2e00720d2d21e25aa
ThomasFzr <thfltzr@gmail.com>
2025-03-19 10:31:40 +0100

    fix: enhance navbar layout and accessibility by adjusting flex properties and image class

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -42,8 +42,8 @@ export const Navbar = () => {
         <div className={`fixed top-0 left-0 w-full z-[100] text-2xl pt-1 text-black items-center transition-transform duration-300 ${visible ? "translate-y-0" : "-translate-y-full"}`}>
             <div className='flex flex-row justify-between w-full py-2 px-4'>
                 <div className="w-[30%] hidden md:flex">
-                    <a href="https://www.instagram.com/lesgriffesduz/" target="_blank" rel="noopener noreferrer">
-                        <img className="w-15 pl-4 hover:scale-110" src="/images/insta.png" alt="Instagram" />
+                    <a className="h-15" href="https://www.instagram.com/lesgriffesduz/" target="_blank" rel="noopener noreferrer">
+                        <img className="w-15 hover:scale-110" src="/images/insta.png" alt="Instagram" />
                     </a>
                 </div>
                 <div>
@@ -52,7 +52,7 @@ export const Navbar = () => {
                     </Link>
                 </div>
 
-                <div className="flex-row justify-around w-[30%] hidden md:flex ">
+                <div className="flex-row justify-around items-center w-[30%] h-15 hidden md:flex ">
                     <Link to="/price" className="hover:scale-110">PRIX</Link>
                     <Link to="/dispo" className="hover:scale-110">DISPO</Link>
                     <Link to="/info" className="hover:scale-110">INFOS</Link>
